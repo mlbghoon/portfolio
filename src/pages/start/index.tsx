5bbb522a6940ef51eaf9bea2fde3cefc903cdf4b
ML <ml@happytalk.io>
2024-03-12 18:30:11 +0900

    [Test] Git Commit Test

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -7,7 +7,7 @@ import { characterState, setName } from 'reduxStore/characterSlice';
 
 const diceFace = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
-
+// test
 
 type diceProp = {
   clicked: boolean;
